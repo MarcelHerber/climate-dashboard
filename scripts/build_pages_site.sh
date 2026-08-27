9f8ad38dc639c322dbb03242afd59ce8ae577a88
MarcelHerber <marcel.herber96@gmail.com>
2026-08-27 18:15:08 +0200

    HYRAS historische Temperaturlogik mit Pages veröffentlichen

--- scripts/build_pages_site.sh
+++ scripts/build_pages_site.sh
@@ -7,6 +7,7 @@ mkdir -p _site/data
 required_root_files=(
   index.html
   monthly_frequency.js
+  hyras_historical_temperature_1km.js
   dekadenrekorde.html
   jahresextremwerte.html
   feiertagsextremwerte.html
